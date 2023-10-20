96d1693ac3e9e3b6bc184da411d85e2289d34855
Phoebe Chang <phoebe03111@gmail.com>
2023-10-19 23:04:46 -0700

    Create home page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Carousel from "@/components/Carousel";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Hero />
+      <Carousel />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
