a8aaecce9225a1b71f6c1e86706d78b33a3dc373
Phoebe Chang <phoebe03111@gmail.com>
2023-10-19 23:32:12 -0700

    Create Navbar and import images

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: "/", key: "home", label: "Home" },
+    { href: "/", key: "how_does_it_work", label: "How Does It Work?" },
+    { href: "/", key: "services", label: "Services" },
+    { href: "/", key: "pricing", label: "Pricing" },
+    { href: "/", key: "contact_us", label: "Contact Us" },
+  ];
