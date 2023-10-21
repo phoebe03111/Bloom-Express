1159366293f07cb8419aca28fae12577f36cdafb
Phoebe Chang <phoebe03111@gmail.com>
2023-10-20 20:45:29 -0700

    Create Features section

--- constants/index.ts
+++ constants/index.ts
@@ -14,3 +14,31 @@ export const PEOPLE_URL = [
   "/person-3.png",
   "/person-4.png",
 ];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Tailor-Made Bouquets",
+    icon: "/flower-icon.png",
+    description:
+      "Personalize your floral expressions with Bloom Express's Bespoke Blooms feature. Choose from a curated selection of locally sourced flowers and create a one-of-a-kind bouquet that perfectly captures the sentiment you wish to convey.",
+  },
+  {
+    title: "Express Petal Delivery",
+    icon: "/flower-icon.png",
+    description:
+      "With our Express Petal Delivery, experience the joy of sending fresh blooms to your loved ones promptly. Bloom Express ensures your thoughtfulness reaches its destination swiftly, making every moment memorable with the timely arrival of nature's beauty.",
+  },
+  {
+    title: "Let Flowers Speak for You",
+    icon: "/flower-icon.png",
+    description:
+      "Feeling lost in the sea of petals? Our Floral Emotion Guide is here to help. Simply express your emotions or the occasion, and let Bloom Express's expert florists guide you in creating a bouquet that beautifully articulates your feelings, turning blooms into a heartfelt message.",
+  },
+  {
+    title: "Subscription Blossoms",
+    icon: "/flower-icon.png",
+    description:
+      "Elevate your space with a continuous infusion of beauty through Bloom Express's Subscription Blossoms feature. Enjoy regularly scheduled flower deliveries, each time bringing the freshness and vibrancy of Bloom Express to your doorstep, making every day a celebration of nature's elegance.",
+  },
+];
