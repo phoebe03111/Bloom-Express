68fbb9ca3bedee6782cba5d18fb4371a22864260
Phoebe Chang <phoebe03111@gmail.com>
2023-10-21 00:30:57 -0700

    Clean up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,7 +32,6 @@ module.exports = {
       backgroundImage: {
         "bg-img-1": "url('/img-3.png')",
         "bg-img-2": "url('/img-4.png')",
-        "feature-bg": "url('/feature-bg.png')",
         pattern: "url('/pattern.png')",
         hero: "url('/img-1.png')",
       },
